97ffeec92225eac7c5206b6442d419908ad5dc7a
rupnarainP <33540132+rupnarainP@users.noreply.github.com>
2020-11-03 23:58:19 +0200

    Add files via upload
    init

---
+++ studentRegistrationApp/src/test/java/com/studentReg/studentRegistrationApp/StudentRegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentReg.studentRegistrationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
